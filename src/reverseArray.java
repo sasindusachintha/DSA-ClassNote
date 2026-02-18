e5e74f69aeb0ac7a842e2acdda8380c6e929d6f9
Sasindu Sachintha <sasindusachintha1234@gmail.com>
2026-02-18 10:07:04 +0530

    added

---
+++ src/reverseArray.java
@@ -0,0 +1,14 @@
+public class reverseArray {
+ public static void main(String[] args){
+     String word = "ICBT";
+     
+     String reversed = " ";
+     
+     for (int i = word.length()-1; i >=0 ;i--){
+         reversed = reversed + word.charAt(i);
+     }
+   System.out.println("Original: "+ word);
+   System.out.println("Reversed: "+ reversed);
+     
+     }    
+}
