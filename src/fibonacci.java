517c122b633d17de750c14863393d088c0306590
sasindu <sasindusachintha1234@gmail.com>
2026-02-18 21:53:42 +0530

    hi

---
+++ src/fibonacci.java
@@ -0,0 +1,16 @@
+public class fibonacci {
+    static int fib(int n){
+        if(n==0) return 0;
+        if(n==1) return 1;
+        
+        return fib(n-1)+ fib(n-2);
+    }    
+    public static void main(String[] args){
+        
+        int n = 6;
+        
+        for(int i= 0; i < n; i++){
+        System.out.print(fib(i)+ " ");            
+        }
+    }
+}
