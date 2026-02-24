9078f95ad0af8b8d03adf61371bf31d355cc2889
sasindusachintha <sasindusachintha1234@gmail.com>
2026-02-24 10:04:06 +0530

    added alphabatical order sort from insertion sort

--- src/insertionAlpha.java
+++ src/insertionAlpha.java
@@ -1,35 +1,38 @@
 
-public class insertionSort {
-
-    public static void insertion(int[] arr) {
+public class insertionAlpha {
+   public static void Stringinsertion(String[] arr) {
         int n = arr.length;
 
         for (int i = 1; i < n; i++) {
-            int key = arr[i];
+            String key = arr[i];
             int j = i - 1;
 
-            while (j >= 0 && arr[j] > key) {
+            while (j >= 0 && arr[j].compareTo(key)> 0) {
                 arr[j + 1] = arr[j];
                 j--;
             }
             arr[j + 1] = key;
         }
     }
-
+   
     public static void main(String[] args) {
-        int[] arr = {10, 3, 4, 23, 56, 20};
+        
+        String[] fruits = {"Apple", "orange", "banana", "Mango", "Pineapple"} ;
+        
 
         System.out.print("Unsorted Array: ");
-        for (int x : arr) {
+        for (String x : fruits) {
             System.out.print(x + " ");
         }
 
         System.out.println();
-        insertion(arr);
+        Stringinsertion(fruits);
         System.out.print("sorted Array: ");
-        for (int x : arr) {
+        for (String x : fruits) {
             System.out.print(x + " ");
         }
         System.out.println();
     }
 }
+
+
