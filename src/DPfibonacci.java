f6613502e87f7ad59becf510f5aa8aa90c14e447
sasindu <sasindusachintha1234@gmail.com>
2026-05-28 13:58:49 +0530

    Clarify loop with comment in DPfibonacci.java
    Added a comment to clarify the loop through the array.

--- src/DPfibonacci.java
+++ src/DPfibonacci.java
@@ -6,7 +6,8 @@ public class DPfibonacci {
       //store values of subproblems (base case)
        dp[0] = 0;
        dp[1] = 1;
-       
+
+      //loop through array
        for(int i = 2 ; i<=n ; i++){
            dp[i] = dp[i-1] + dp[i-2];
        }
