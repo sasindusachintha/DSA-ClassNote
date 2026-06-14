64675a69dae4cc587c16a52d334ef7f3239edd0e
sasindu <sasindusachintha1234@gmail.com>
2026-06-14 13:23:12 +0530

    Update DPfibonacci.java

--- src/DPfibonacci.java
+++ src/DPfibonacci.java
@@ -18,7 +18,8 @@ public class DPfibonacci {
    public static void main(String[] args){
        
        int n = 6;
-       
+
+      // print values 
        for(int i = 0 ; i < n ;i++){
            System.out.print(fib(i)+ " ");
        }
