ebc5d34cde393931a51269319760de174989a95c
sasindu <sasindusachintha1234@gmail.com>
2026-08-14 11:29:20 +0530

    Change Fibonacci sequence limit from 6 to 7

--- src/fibonacci.java
+++ src/fibonacci.java
@@ -7,7 +7,7 @@ public class fibonacci {
     }    
     public static void main(String[] args){
         
-        int n = 6;
+        int n = 7;
         
         for(int i= 0; i < n; i++){
         System.out.print(fib(i)+ " ");            
