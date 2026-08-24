26f520b47b4d400c925537a8570fd908cf2a1eba
sasindu <sasindusachintha1234@gmail.com>
2026-08-25 00:09:39 +0530

    Update GraphMatrix.java

--- src/GraphMatrix.java
+++ src/GraphMatrix.java
@@ -36,7 +36,7 @@ public class GraphMatrix {
          graph.addEdge(0, 1);
          graph.addEdge(0, 2);
          graph.addEdge(1, 3);
-         graph.addEdge(0, 2);
+         graph.addEdge(0, 4);
          graph.addEdge(2, 3);
          
          graph.printGraph();
