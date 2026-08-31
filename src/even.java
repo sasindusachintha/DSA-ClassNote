02a102c6fccc0c89dd19e2f4a304293d4934302e
sasindu <sasindusachintha1234@gmail.com>
2026-08-31 12:55:23 +0530

    Calculate and print sum of even numbers less than 100
    Added a sum variable to calculate the sum of even numbers.

--- src/even.java
+++ src/even.java
@@ -1,10 +1,13 @@
 // print even numbers less than 100
 public class even {
      public static void main(String[] args){
+         
+         //sum 
          int sum = 0;
          for(int i= 0; i <= 100 ; i+=2){
              sum = sum + i;
          }
+          
          //print
          System.out.println(sum);
      }    
