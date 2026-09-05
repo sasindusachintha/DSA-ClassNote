7b5c6f0994925d4d995c912e915f9aa4dcae4667
sasindu <sasindusachintha1234@gmail.com>
2026-09-05 12:10:09 +0530

    Add comment for number of Fibonacci terms in main
    Added a comment to specify the number of Fibonacci terms to generate.

--- src/fibonachchi.java
+++ src/fibonachchi.java
@@ -2,6 +2,8 @@
 // Iterative method to generate Fibonacci series
 public class fibonachchi {
    public static void main(String[] args){
+
+      // Number of Fibonacci terms to generate
        int n = 100;
        int a =0;
        int b = 1;
