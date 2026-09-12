a2cf14109db5b855f7654e2438e2b650744db7db
sasindu <sasindusachintha1234@gmail.com>
2026-09-12 12:25:59 +0530

    Update fibonacci.java

--- src/fibonacci.java
+++ src/fibonacci.java
@@ -1,7 +1,8 @@
 // fibonacci 
 public class fibonacci {
+
     static int fib(int n){
-        if(n==0) return 0;
+        if(n==0) return 0;//base case 1
         if(n==1) return 1;
         
         return fib(n-1)+ fib(n-2);
