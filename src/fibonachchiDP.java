10670e4b86850e0db3b7dadd87065b4fcf7123c5
sasindu <sasindusachintha1234@gmail.com>
2026-09-17 22:23:03 +0530

    Update fibonachchiDP.java

--- src/fibonachchiDP.java
+++ src/fibonachchiDP.java
@@ -23,7 +23,8 @@ public class fibonachchiDP {
    public static void main(String[] args){
        
        int n =10;
-               
+        
+      //for loop       
        for(int i= 0 ; i< n ; i++){
        System.out.print(fib(i)+" ");
    }
