2b6c83d8ea8bbfc034405d30fb5cd51e01488ed2
sasindu <sasindusachintha1234@gmail.com>
2026-09-17 22:22:03 +0530

    Update fibonachchiDP.java

--- src/fibonachchiDP.java
+++ src/fibonachchiDP.java
@@ -12,6 +12,7 @@ public class fibonachchiDP {
        dp[0] = 0;
        dp[1]  = 1; // base case 
        
+       // for loop
        for(int i = 2; i <= n ; i++){
            dp[i] = dp[i-1] + dp[i-2];
        }
